8f69854dd0a0d07b27a95a2faf3724c1374ce1df
xxparthparekhxx <79994063+xxparthparekhxx@users.noreply.github.com>
2021-05-02 17:34:41 +0530

    Update redditleacher.py

--- redditleacher.py
+++ redditleacher.py
@@ -1,3 +1,12 @@
 import praw, requests, os, shutil, time, pathlib, random, glob, json,PIL
 from instabot import Bot
 from PIL import Image
+
+def reddit_initializer(Client_Id, Client_Secret):
+    reddit = praw.Reddit(
+        client_id=Client_Id,
+        client_secret=Client_Secret,
+        user_agent="bot 0.1",
+    )
+    print("reddit is ready?..." + str(reddit.read_only))
+    return reddit
