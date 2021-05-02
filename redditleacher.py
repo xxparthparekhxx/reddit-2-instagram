18331041fa0a68625d09f6f2c8630574991f2570
xxparthparekhxx <79994063+xxparthparekhxx@users.noreply.github.com>
2021-05-02 17:35:43 +0530

    Update redditleacher.py

--- redditleacher.py
+++ redditleacher.py
@@ -2,6 +2,7 @@ import praw, requests, os, shutil, time, pathlib, random, glob, json,PIL
 from instabot import Bot
 from PIL import Image
 
+
 def reddit_initializer(Client_Id, Client_Secret):
     reddit = praw.Reddit(
         client_id=Client_Id,
@@ -10,3 +11,24 @@ def reddit_initializer(Client_Id, Client_Secret):
     )
     print("reddit is ready?..." + str(reddit.read_only))
     return reddit
+
+
+def getcredentials():
+    if "creds.json" not in os.listdir():
+        print("First time setup! \n enter your credentials below")
+        credentials = {
+            "Reddit_client_id": input("enter your reddit_client_id"),
+            "Reddit_client_secret": input("enter your reddit client secret"),
+            "Instagram_id": input("enter your Insragram id #locally stored"),
+            "Instagam_password": input("enter your Instagram password"),
+        }
+        with open("creds.json", "w") as f:
+            json.dump(credentials, f)
+        return credentials
+    elif "creds.json" in os.listdir():
+        print("retriving credentials")
+        with open("creds.json", "r") as f:
+            credentials = json.load(f)
+        return credentials
+
+
